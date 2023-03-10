bfb7e440a27add48101f9208b1183931f378d74f
Admin <Peer@kieromin-hh.de>
2023-03-10 17:26:24 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;/**
+ * @author GEGENPHASE
+ * @date ${DAY}.${MONTH}.${YEAR} ${HOUR}:${MINUTE}
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
